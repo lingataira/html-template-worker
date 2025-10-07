fa65d5b6293c2f256510ef20e5355aef6f8e5de3
Aira Lingat <airal@superpatch.com>
2025-10-07 14:03:09 -0400

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 import template from '../template';
-import anotherTemplate from '../anotherTemplate';
+import anotherTemplate from '../another-template';
 
 export default {
 	async fetch(request, env) {
