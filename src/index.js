7385b4c459a3bff0a79c0bc9dedf74ccc70ca397
Aira Lingat <airal@superpatch.com>
2025-10-07 13:58:41 -0400

    test

--- src/index.js
+++ src/index.js
@@ -7,23 +7,26 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import template from '../template';
+import template from "../template";
 
 export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 
-		if (url.pathname.startsWith('/assets/')) {
-			const assetPath = url.pathname.replace(/^\/assets/, '');
-			const assetRequest = new Request(assetPath, request);
+		if (url.pathname.startsWith("/assets/")) {
+			const assetPath = url.pathname.replace(/^\/assets/, "");
+			const assetUrl = new URL(assetPath, request.url);
+			const assetRequest = new Request(assetUrl, request);
+
 			return env.ASSETS.fetch(assetRequest);
 		}
 
 		return new Response(template(), {
-			headers: { 'content-type': 'text/html' },
+			headers: { "content-type": "text/html" },
 		});
 	},
 };
+
 
 // export default {
 // 	async fetch(request, env, ctx) {
