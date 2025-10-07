bf557a9d1862ecadd28a45e2cbc9173ad1f4496f
Aira Lingat <airal@superpatch.com>
2025-10-07 15:41:06 -0400

    test

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ export default {
 		const url = new URL(request.url);
 
 		if (url.pathname.startsWith('/assets/')) {
-			const assetPath = url.pathname;
-			return env.ASSETS.fetch(assetPath);
+			const assetRequest = new Request(url.pathname, request);
+			return env.ASSETS.fetch(assetRequest);
 		}
 
 		if (url.pathname.startsWith('/css/')) {
